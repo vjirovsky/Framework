62716ad978daa8544f1ce340be0516c101b14e54
Tomáš Votruba <tomas.vot@gmail.com>
2013-10-25 23:26:06 +0200

    Js/css cleanup

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,26 +1,28 @@
 $(document).ready(function() {
 
+	// tooltip
+	$('[rel=tooltip]').tooltip();
+
+
 	// form on change submit
 	$(function () {
-		$("input.onChangeSubmit").change(function () {
-			$(this).closest("form").submit();
+		$('input.onChangeSubmit').change(function () {
+			$(this).closest('form').submit();
 		});
 	});
 
 	// confirm alert
-	$("*[data-confirm]").on("click", function() {
-		return confirm($(this).data("confirm"));
+	$('*[data-confirm]').on('click', function() {
+		return confirm($(this).data('confirm'));
 	});
 
 	// hide flashes
 	window.setTimeout(function() {
-		$(".alert.timeout").fadeTo(500, 0).slideUp(500, function(){
+		$('.alert.timeout').fadeTo(500, 0).slideUp(500, function(){
 			$(this).remove();
 		});
 	}, 2000);
-
 });
-
 
 jQuery(window).load(function () {
 	jQuery.nette.ext('init').linkSelector = 'a.ajax';
