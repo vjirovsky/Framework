f2eb85a239cd46180f6fc2b9641b4bd8e8a8acc8
Tomáš Votruba <tomas.vot@gmail.com>
2013-11-12 10:53:19 +0100

    Routing: PairsRoute decomposed to TIdRoute

--- Application/Routers/PairsRoute.php
+++ Application/Routers/PairsRoute.php
@@ -2,44 +2,14 @@
 
 namespace Schmutzka\Application\Routers;
 
-use Models;
 use Nette;
-use Nette\Application\Request;
+use Nette\Application\Routers\Route;
 use Nette\Utils\Strings;
-use Nette\Http\Url;
-use Schmutzka;
-use Schmutzka\Utils\Arrays;
-use Schmutzka\Utils\Name;
 
 
-/**
- * @use: $frontRouter[] = new PairsRoute('predmet/<id>', 'Subject:detail', $this->subjectModel, $this->cache, ['id', 'url']);
- */
-class PairsRoute extends Nette\Application\Routers\Route
+class PairsRoute extends Route
 {
-	/** @var Models\Base */
-	private $model;
-
-	/** @var array */
-	private $columns;
-
-	/** @var string */
-	private $primaryKey;
-
-	/** @var string */
-	private $secondaryKey;
-
-	/** @var Nette\Caching\Cache */
-	private $cache;
-
-	/** @var string */
-	private $cacheTag;
-
-	/** @var string */
-	private $mask;
-
-	/** @var string */
-	private $metadata;
+	use TIdRoute;
 
 
 	/**
@@ -47,151 +17,37 @@ class PairsRoute extends Nette\Application\Routers\Route
 	 * @param array
 	 * @param Models\Base
 	 * @param Nette\Caching\Cache
-	 * @param array
+	 * @param string
+	 * @param string
 	 */
-	public function __construct($mask, $metadata = [], $model, Nette\Caching\Cache $cache, $columns = ['id', 'name'])
+	public function __construct($mask, $metadata = [], $model, Nette\Caching\Cache $cache, $keyName = 'id', $valueName = 'name')
 	{
 		$this->mask = $mask;
 		$this->metadata = $metadata;
 		$this->model = $model;
 		$this->cache = $cache;
-		list($this->primaryKey, $this->secondaryKey) = $columns;
+		$this->keyName = $keyName;
+		$this->valueName = $valueName;
 
-		$this->cacheTag = 'route_' . sha1($mask);
-
-		parent::__construct($mask, $metadata, 0);
+		parent::__construct($mask, $metadata);
 	}
 
 
 	/**
-	 * Maps HTTP request to a Request object.
-	 * @return HttpRequest|NULL
+	 * @return  array [key => value]
 	 */
-	public function match(Nette\Http\IRequest $httpRequest)
+	public function buildTranslateTable()
 	{
-		$appRequest = parent::match($httpRequest);
+		$result = $this->model->fetchAll();
 
-		if ( ! $appRequest) {
-			return $appRequest;
+		$data = [];
+		foreach ($result as $key => $row) {
+			$key = $row[$this->keyName];
+			$value = Strings::webalize($row[$this->valueName]);
+			$data[$key] = $value;
 		}
 
-		$keyParam = $appRequest->parameters[$this->primaryKey];
-		if (!empty($keyParam)) {
-			$id = $this->getByValue($keyParam);
-			if ($id === NULL) {
-				return NULL;
-			}
-
-			$params = $appRequest->parameters;
-			$params[$this->primaryKey] = $id;
-			$appRequest->parameters = $params;
-		}
-
-		return $appRequest;
-	}
-
-
-	/**
-	 * Constructs absolute URL from Request object.
-	 * @param  Request
-	 * @param  Url
-	 * @return string|NULL
-	 */
-	public function constructUrl(Request $appRequest, Url $refUrl)
-	{
-		if (isset($appRequest->parameters[$this->primaryKey])) {
-			$params = $appRequest->parameters;
-			$keyParam = $params[$this->primaryKey];
-
-			// check if router matches (also router presenter)
-			$mpv = explode(':', $this->metadata);
-			$action = array_pop($mpv);
-
-			if (($params['action'] == $action) && $keyParam) {
-				$value = $this->getByKey($keyParam);
-
-				if ($value === NULL) {
-					return NULL;
-				}
-
-
-				$params = $appRequest->parameters;
-				$params[$this->primaryKey] = $value;
-				$appRequest->parameters = $params;
-			}
-		}
-
-		return parent::constructUrl($appRequest, $refUrl);
-	}
-
-
-	/********************** helpers **********************/
-
-
-	/**
-	 * Get cached list
-	 * @return array
- 	 */
-	private function getPairList()
-	{
-		$pairList = $this->cache->load($this->cacheTag);
-
-		if ($pairList == NULL) {
-			$pairList = $this->model->fetchPairs($this->primaryKey, $this->secondaryKey);
-
-			foreach ($pairList as $key => $value) {
-				$pairList[$key] = Strings::webalize($value);
-			}
-
-			$this->cache->save($this->cacheTag, $pairList, [
-				'tag' => $this->cacheTag,
-				'expire' => '30 mins'
-			]);
-		}
-
-		return $pairList;
-	}
-
-
-	/**
-	 * Get cached list flipped
-	 * @return  array
-	 */
-	private function getPairListFlipped()
-	{
-		$pairList = $this->getPairList();
-		return array_flip($pairList);
-	}
-
-
-	/**
-	 * @param  int
-	 * @return  string|NULL
-	 */
-	private function getByKey($key)
-	{
-		$pairList = $this->getPairList();
-
-		if (isset($pairList[$key])) {
-			return $pairList[$key];
-		}
-
-		return NULL;
-	}
-
-
-	/**
-	 * @param  string
-	 * @return  srting|NULL
-	 */
-	private function getByValue($value)
-	{
-		$pairListFlipped = $this->getPairListFlipped();
-		if (isset($pairListFlipped[$value])) {
-			return $pairListFlipped[$value];
-		}
-
-		return NULL;
+		return $data;
 	}
 
 }
---
+++ Application/Routers/TIdRoute.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Schmutzka\Application\Routers;
+
+use Nette;
+
+
+trait TIdRoute
+{
+	/** @var string */
+	private $mask;
+
+	/** @var string */
+	private $metadata;
+
+	/** @var Schmutzka\Models\* */
+	private $model;
+
+	/** @var Nette\Caching\Cache */
+	private $cache;
+
+	/** @var string */
+	private $keyName;
+
+	/** @var string */
+	private $valueName;
+
+
+	/**
+	 * Maps HTTP request to a Request object.
+	 * @return HttpRequest|NULL
+	 */
+	public function match(Nette\Http\IRequest $httpRequest)
+	{
+		$appRequest = parent::match($httpRequest);
+
+		if ( ! $appRequest) {
+			return $appRequest;
+		}
+
+		$keyParam = $appRequest->parameters['id'];
+
+		if (!empty($keyParam)) {
+			$id = $this->getKeyByValue($keyParam);
+
+			if ($id === NULL) {
+				return NULL;
+			}
+
+			$params = $appRequest->parameters;
+			$params['id'] = $id;
+			$appRequest->parameters = $params;
+		}
+
+		return $appRequest;
+	}
+
+
+	/**
+	 * Constructs absolute URL from Request object.
+	 * @param  Request
+	 * @param  Url
+	 * @return string|NULL
+	 */
+	public function constructUrl(Nette\Application\Request $appRequest, Nette\Http\Url $refUrl)
+	{
+		if (isset($appRequest->parameters[$this->keyName])) {
+			$params = $appRequest->parameters;
+			$keyParam = $params[$this->keyName];
+
+			// check if router matches (also router presenter)
+			$mpv = explode(':', $this->metadata);
+			$action = array_pop($mpv);
+
+			if (($params['action'] == $action) && $keyParam) {
+				$value = $this->getValueByKey($keyParam);
+
+				if ($value === NULL) {
+					return NULL;
+				}
+
+				$params = $appRequest->parameters;
+				$params[$this->keyName] = $value;
+				$appRequest->parameters = $params;
+			}
+		}
+
+		return parent::constructUrl($appRequest, $refUrl);
+	}
+
+
+	/**
+	 * @return array
+ 	 */
+	private function getTranslateTable()
+	{
+		$cacheTag = 'route_' . sha1($this->mask . $this->metadata);
+		$data = $this->cache->load($cacheTag);
+
+		if ($data == NULL) {
+			$data = $this->buildTranslateTable();
+
+			$this->cache->save($cacheTag, $data, [
+				'tag' => $cacheTag,
+				'expire' => '30 mins'
+			]);
+		}
+
+		return $data;
+	}
+
+
+	/**
+	 * @param  int
+	 * @return  string|NULL
+	 */
+	private function getValueByKey($key)
+	{
+		$data = $this->getTranslateTable();
+
+		if (isset($data[$key])) {
+			return $data[$key];
+		}
+
+		return NULL;
+	}
+
+
+	/**
+	 * @param  string
+	 * @return  int|NULL
+	 */
+	private function getKeyByValue($value)
+	{
+		$data = $this->getTranslateTableFlipped();
+
+		if (isset($data[$value])) {
+			return $data[$value];
+		}
+
+		return NULL;
+	}
+
+
+	/**
+	 * @return  array [ value => key ]
+	 */
+	private function getTranslateTableFlipped()
+	{
+		$data = $this->getTranslateTable();
+		return array_flip($data);
+	}
+
+}
