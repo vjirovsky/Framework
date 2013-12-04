9c53455d8b45b3000e95d15eb9e3ac57ed84ebfc
Tomáš Votruba <tomas.vot@gmail.com>
2013-12-04 23:24:28 +0100

    RegistrationControl simplified

--- Components/Registration/RegistrationControl.php
+++ Components/Registration/RegistrationControl.php
@@ -2,28 +2,16 @@
 
 namespace Components;
 
-use Nette;
-use Nette\DateTime;
-use Nette\Utils\Strings;
-use Schmutzka;
 use Schmutzka\Application\UI\Control;
 use Schmutzka\Application\UI\Form;
-use Schmutzka\Mail\Message;
 use Schmutzka\Security\UserManager;
 
 
 /**
- * @method setSendSuccessEmail(bool)
  * @method setRole(string)
  */
 class RegistrationControl extends Control
 {
-	/** @inject @var Nette\Mail\IMailer */
-	public $mailer;
-
-	/** @inject @var Schmutzka\Mail\IMessage */
-	public $message;
-
 	/** @inject @var Models\User */
 	public $userModel;
 
@@ -36,10 +24,6 @@ class RegistrationControl extends Control
 	/** @inject @var Schmutzka\ParamService */
 	public $paramService;
 
-
-	/** @var bool */
-	private $sendSuccessEmail = FALSE;
-
 	/** @var string */
 	private $role;
 
@@ -50,19 +34,19 @@ class RegistrationControl extends Control
 
 		$form = new Form;
 
-		$form->addText('email', $this->paramService->form->email->label)
-			->addRule(Form::FILLED, $this->paramService->form->email->ruleFilled)
-			->addRule(Form::EMAIL, $this->paramService->form->email->ruleFormat)
+		$form->addText('email', 'Email')
+			->addRule(Form::FILLED, 'Zadejte email')
+			->addRule(Form::EMAIL, 'Opravte formáte emailu')
 			->addRule(function ($input) use ($userModel) {
 				return ! $userModel->fetch(['email' => $input->value]);
-			}, $this->paramService->form->email->alreadyExists);
+			}, 'Tento email je již registrován. Použijte jiný.');
 
-		$form->addPassword('password', $this->paramService->form->password->label)
-			->addRule(Form::FILLED, $this->paramService->form->password->ruleFilled)
-			->addRule(Form::MIN_LENGTH, $this->paramService->form->password->length, 5);
+		$form->addPassword('password', 'Heslo')
+			->addRule(Form::FILLED, 'Zadejte heslo')
+			->addRule(Form::MIN_LENGTH, 'Heslo musí mít aspoň %d znaků', 5);
 
-		$form->addSubmit('send', $this->paramService->form->send->register)
-			->setAttribute('class', 'btn btn-primary');
+		$form->addSubmit('send', 'Registrovat se')
+			->setAttribute('class', 'btn btn-success');
 
 		return $form;
 	}
@@ -79,34 +63,11 @@ class RegistrationControl extends Control
 
 		$this->userManager->register($values);
 
-		if ($this->sendSuccessEmail) {
-			$this->sendSuccessEmail($values);
-		}
-
 		$loginColumn = (isset($this->paramService->loginColumn) ? $this->paramService->loginColumn : 'email');
 		$this->user->login($values[$loginColumn], $rawValues['password']);
+
 		$this->presenter->flashMessage($this->paramService->registration->onSuccessAndLogin, 'success');
-
-
 		$this->redirect('this');
-	}
-
-
-	/**
-	 * @param array
-	 */
-	private function sendSuccessEmail($values)
-	{
-		if ( ! isset($this->paramService->email->from)) {
-			throw new \Exception('Missing param email.from');
-		}
-
-		$message = $this->message->create();
-		$message->setFrom($this->paramService->email->from);
-		$message->addTo($values['email']);
-		$message->addCustomTemplate('registration', $values);
-
-		$this->mailer->send($message);
 	}
 
 }
