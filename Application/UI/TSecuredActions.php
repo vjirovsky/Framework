51c300b1f786e08e324f8d8d0149d2034890e245
Tomáš Votruba <tomas.vot@gmail.com>
2013-12-05 15:56:44 +0100

    TAnnotations + TSecuredActions enhanced

--- Application/UI/TSecuredActions.php
+++ Application/UI/TSecuredActions.php
@@ -5,14 +5,18 @@ namespace Schmutzka\Application\UI;
 use Nette;
 use Nette\Reflection;
 use Nette\Security\User;
+use Schmutzka;
 
 
 trait TSecuredActions
 {
+	use Schmutzka\TAnnotations;
+
 
 	public function beforeRender()
 	{
 		$this->authCheck();
+		$this->unloggedCheck();
 		parent::beforeRender();
 	}
 
@@ -23,50 +27,24 @@ trait TSecuredActions
 	protected function authCheck()
 	{
 		$annotation = 'secured';
+		$authenticate = FALSE;
 
-		$actionMethod = $this->formatActionMethod($this->action);
-		$signalMethod = $this->formatSignalMethod($this->signal);
-		$renderMethod = $this->formatRenderMethod($this->view);
-
-		if ($this->hasMethodAnnotation($actionMethod, $annotation)) {
+		if ($flag = $this->getViewAnnotation($this->view, $annotation)) {
 			$authenticate = TRUE;
-			$reflection = Reflection\Method::from($this, $actionMethod);
-			$flag = $this->getAnnotation($reflection, $annotation);
 
-		} elseif ($this->isSignalReceiver($this) && $this->hasMethodAnnotation($signalMethod, $annotation)) {
+		} elseif ($flag = $this->getPresenterAnnotation($annotation)) {
 			$authenticate = TRUE;
-			$reflection = Reflection\Method::from($this, $signalMethod);
-			$flag = $this->getAnnotation($reflection, $annotation);
-
-		} elseif ($this->hasMethodAnnotation($renderMethod, $annotation)) {
-			$authenticate = TRUE;
-			$reflection = Reflection\Method::from($this, $renderMethod);
-			$flag = $this->getAnnotation($reflection, $annotation);
-
-		} elseif ($this->hasAnnotation($annotation)) {
-			$authenticate = TRUE;
-			$flag = $this->getAnnotation($this->getReflection(), $annotation);
-
-		} else {
-			$authenticate = FALSE;
-			$flag = NULL;
-		}
-
-		if (isset($reflection)) {
-			$flags = $reflection->getAnnotations();
 		}
 
 		if ($authenticate) {
 			if ( ! $this->user->isLoggedIn()) {
-				$this->flashMessage('Sign in first', 'warning');
-				// $this->flashMessage('Pro přístup k této operaci se prosím přihlaste.', 'warning');
+				$this->flashMessage('Pro přístup k této operaci se prosím přihlaste.', 'error');
 				$backlink = $this->presenter->storeRequest();
 				$this->redirect('Auth:signIn', ['backlink' => $backlink]);
 
-			} elseif (isset($flags['role'])) {
-				if (in_array($this->user->role, (array) $flags['role'][0]) == FALSE) {
-					$this->flashMessage('Forbidden access.', 'error');
-					// $this->flashMessage('Nemáte oprávnění pro přístup k této operaci.', 'error');
+			} elseif (isset($flag['role'])) {
+				if (in_array($this->user->role, (array) $flag['role'][0]) == FALSE) {
+					$this->flashMessage('Nemáte oprávnění pro přístup k této operaci.', 'error');
 					$this->redirect('Homepage:default');
 				}
 			}
@@ -75,50 +53,15 @@ trait TSecuredActions
 
 
 	/**
-	 * Checks if class has a given annotation
-	 * @param string $annotation
-	 * @return bool
+	 * Check if user is NOT logged.
+	 * Redirect elsewhere.
 	 */
-	protected function hasAnnotation($annotation)
+	protected function unloggedCheck()
 	{
-		return $this->getReflection()->hasAnnotation($annotation);
-	}
+		$annotation = 'unlogged';
 
-
-	/**
-	 * Checks if given method has a given annotation
-	 * @param string $method
-	 * @param string $annotation
-	 * @return bool
-	 */
-	protected function hasMethodAnnotation($method, $annotation)
-	{
-		if ( ! $this->getReflection()->hasMethod($method)) return FALSE;
-
-		$rm = Reflection\Method::from($this->getReflection()->getName(), $method);
-		return $rm->hasAnnotation($annotation);
-	}
-
-
-	/**
-	 * Get all anotations of given name
-	 * @param object $reflection
-	 * @param string $name
-	 * @return mixed
-	 */
-	protected function getAnnotation($reflection, $name)
-	{
-		$res = $reflection->getAnnotations();
-		if (isset($res[$name]))	{
-			if (sizeof($res[$name]) > 1) {
-				return $res[$name];
-
-			} else {
-				return end($res[$name]);
-			}
-
-		} else {
-			return NULL;
+		if ($this->user->loggedIn && ($this->getPresenterAnnotation($annotation) || $this->getViewAnnotation($this->view, $annotation))) {
+			$this->redirect('Homepage:default');
 		}
 	}
 
---
+++ common/TAnnotations.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Schmutzka;
+
+
+trait TAnnotations
+{
+	/** @var Nette\Reflection\ClassType */
+	private $reflection;
+
+
+	/**
+	 * @param  string
+	 * @param  bool|array
+	 */
+	protected function getPresenterAnnotation($annotation)
+	{
+		$ref = $this->getRef();
+
+		if ($ref->hasAnnotation($annotation)) {
+			return $ref->getAnnotation($annotation);
+		}
+
+		return FALSE;
+	}
+
+
+	/**
+	 * @param  string
+	 * @param  string
+	 * @return bool|string|array
+	 */
+	protected function getViewAnnotation($view, $annotation)
+	{
+		if ($flag = $this->getMethodAnnotation('action' . ucfirst($view), $annotation)) {
+			return $flag;
+
+		} elseif ($flag = $this->getMethodAnnotation('render' . ucfirst($view), $annotation)) {
+			return $flag;
+		}
+
+		return FALSE;
+	}
+
+
+	/**
+	 * @param  string
+	 * @param  string
+	 * @return bool|string|array
+	 */
+	protected function getMethodAnnotation($method, $annotation)
+	{
+		$ref = $this->getRef();
+
+		if ($ref->hasMethod($method)) {
+			if ($refMethod = $ref->getMethod($method)->hasAnnotation($annotation)) {
+				if ($flag = $ref->getAnnotation($annotation)) {
+					return $flag;
+				}
+
+				return TRUE;
+			}
+		}
+
+		return FALSE;
+	}
+
+
+	/**
+	 * @return  Nette\Reflection\ClassType
+	 */
+	protected function getRef()
+	{
+		if ($this->reflection == NULL) {
+			$this->reflection = $this->getReflection();
+		}
+
+		return $this->reflection;
+	}
+
+}
